5bdeee791d617180a81732a48c3e03ce4ad739b4
carlosGomezMojica <gomezmojicacarlosmanuel@gmail.com>
2025-08-29 23:39:50 -0500

    changes themes for select themeHUB

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,77 +1,15 @@
 return {
-  -- Solarized-Osaka Theme
-  {
-    "craftzdog/solarized-osaka.nvim",
-    config = function()
-      require("solarized-osaka").setup({
-        transparent = true,
-        terminal_colors = true,
-        styles = {
-          comments = { italic = true },
-          keywords = { italic = true },
-          functions = {},
-          variables = {},
-          sidebars = "dark",
-          floats = "dark",
-        },
-        sidebars = { "qf", "help" },
-        day_brightness = 0.3,
-        hide_inactive_statusline = false,
-        dim_inactive = false,
-        lualine_bold = false,
-      })
-    end,
-  },
-
-  -- Catppuccin Theme
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "frappe",
-        transparent_background = true,
-        integrations = {
-          treesitter = true,
-          telescope = true,
-          cmp = true,
-          gitsigns = true,
-        },
-      })
-    end,
-  },
-
-  -- Gruvbox Theme
-  {
-    "morhetz/gruvbox",
-    lazy = false,
-    priority = 1000,
-  },
-
-  -- LazyVim Configuration (Predeterminar gruvbox aquí si quieres)
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox", -- <- Puedes cambiarlo por "catppuccin" o "solarized-osaka"
-    },
-  },
-
-  -- Toggle Theme Command
-  {
+  "erl-koenig/theme-hub.nvim",
+  dependencies = {
     "nvim-lua/plenary.nvim",
-    config = function()
-      local themes = { "solarized-osaka", "catppuccin", "gruvbox" }
-      local current_theme_index = 1
-
-      function ToggleTheme()
-        current_theme_index = current_theme_index % #themes + 1
-        local new_theme = themes[current_theme_index]
-        vim.cmd("colorscheme " .. new_theme)
-        print("Theme changed to: " .. new_theme)
-      end
-
-      vim.api.nvim_create_user_command("ToggleTheme", ToggleTheme, {})
-    end,
+    -- Optional: for themes that use lush (will be notified if a theme requires it)
+    -- "rktjmp/lush.nvim"
   },
+  config = function()
+    require("theme-hub").setup({
+      auto_install_on_select = true,
+      apply_after_install = true,
+      persistent = true,
+    })
+  end,
 }
