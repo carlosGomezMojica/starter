c664853bed629f2d207b463d09cd0dc7dfd420fc
carlosGomezMojica <gomezmojicacarlosmanuel@gmail.com>
2026-03-28 20:04:22 -0500

    add agentic.lua in plugins.

---
+++ lua/plugins/agentic.lua
@@ -0,0 +1,62 @@
+return {
+  "carlos-algms/agentic.nvim",
+  keys = {
+    -- abrir/cerrar chat
+    {
+      "<leader>aa",
+      function()
+        require("agentic").toggle()
+      end,
+      desc = "Agentic Toggle",
+    },
+
+    -- nueva sesión
+    {
+      "<leader>an",
+      function()
+        require("agentic").new_session()
+      end,
+      desc = "Agentic New Session",
+    },
+
+    -- restaurar sesión
+    {
+      "<leader>ar",
+      function()
+        require("agentic").restore_session()
+      end,
+      desc = "Agentic Restore",
+    },
+
+    -- enviar archivo actual
+    {
+      "<leader>af",
+      function()
+        require("agentic").add_file()
+      end,
+      desc = "Send File to Agent",
+    },
+
+    -- enviar selección visual
+    {
+      "<leader>as",
+      function()
+        require("agentic").add_selection()
+      end,
+      mode = "v",
+      desc = "Send Selection",
+    },
+
+    -- enviar diagnósticos del buffer
+    {
+      "<leader>ad",
+      function()
+        require("agentic").add_buffer_diagnostics()
+      end,
+      desc = "Send Diagnostics",
+    },
+  },
+  opts = {
+    provider = "codex-acp",
+  },
+}
