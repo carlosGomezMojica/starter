f6504db7512b305745b8baa3bf1f125302009bf4
carlosGomezMojica <gomezmojicacarlosmanuel@gmail.com>
2026-04-18 09:56:28 -0500

    use system themes update.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,15 +1,1 @@
-return {
-  "erl-koenig/theme-hub.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    -- Optional: for themes that use lush (will be notified if a theme requires it)
-    -- "rktjmp/lush.nvim"
-  },
-  config = function()
-    require("theme-hub").setup({
-      auto_install_on_select = true,
-      apply_after_install = true,
-      persistent = true,
-    })
-  end,
-}
+/home/withclay/.config/omarchy/current/theme/neovim.lua
